e9c0bad3ec27bad11eafb4836a2ec8c1fd1e0e15
Nizam uddin Sangram <nizamuddinstruggle@gmail.com>
2024-07-06 22:50:19 +0600

    add a service card dynamic

--- src/components/Homepage/Services.jsx
+++ src/components/Homepage/Services.jsx
@@ -5,7 +5,7 @@ const Services = () => {
   return (
     <div className="grid mx-auto container grid-cols-1 mt-12 lg:grid-cols-3 gap-6">
       {services.map((service) => (
-        <ServicesCard service={service} key={service.id} />
+        <ServicesCard service={service} key={service._id} />
       ))}
     </div>
   );
--- src/components/cards/ServicesCard.jsx
+++ src/components/cards/ServicesCard.jsx
@@ -1,17 +1,17 @@
+import Image from "next/image";
+
 const ServicesCard = ({ service }) => {
+  const { title, img, price } = service || {};
   return (
     <div className="grid mx-auto container grid-cols-1 mt-12 lg:grid-cols-3 gap-6">
       <div className="card card-compact bg-base-100 w-96 shadow-xl">
         <figure>
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-            alt="Shoes"
-          />
+          <Image src={img} width={430} height={100} alt="Shoes" />
         </figure>
         <div className="card-body">
-          <h2 className="card-title">Shoes!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div className="card-actions justify-end">
+          <h2 className="card-title">{title}</h2>
+          <div className="card-actions justify-between items-center">
+            <h6 className="text-primary text-lg font-bold">price: ${price}</h6>
             <button className="btn btn-primary">Buy Now</button>
           </div>
         </div>
